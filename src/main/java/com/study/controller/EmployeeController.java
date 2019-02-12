bd9a56ed480d464c59f6e970258b84383a526ce7
wangxingang <wangxingang0610@sina.com>
2019-02-12 16:16:45 +0800

    问题：     提交的数据格式不对，则会报错

--- src/main/java/com/study/controller/EmployeeController.java
+++ src/main/java/com/study/controller/EmployeeController.java
@@ -1,11 +1,16 @@
 package com.study.controller;
 
+import com.study.dao.DepartmentDao;
 import com.study.dao.EmployeeDao;
+import com.study.entities.Department;
 import com.study.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import javax.jws.WebParam;
 import java.util.Collection;
@@ -16,6 +21,9 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     //员工列表
     @GetMapping("/emps")
     public String emps(Model model){
@@ -24,4 +32,22 @@ public class EmployeeController {
         model.addAttribute("emps",employees);
         return "emp/list";
     }
+
+    //员工添加页面
+    @GetMapping("/toAdd")
+    public String toAdd(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+
+        return "emp/add";
+    }
+
+    //员工添加
+    @PostMapping("/save")
+    public String add(Employee employee){
+        System.out.println(employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
 }
